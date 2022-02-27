e23ba2039c0ed3f2b604a43634a7379201c426b8
parsa safaei <100072841+Parsa-safaei@users.noreply.github.com>
2022-02-27 23:42:35 +0330

    Add files via upload

---
+++ PYw02ex6pg63.py
@@ -0,0 +1,13 @@
+x = 2
+(a) : print("x")
+# x is printed as a string
+(b) :print('x')
+# same as (a)
+(c) : print(x)
+# the value is going to be printed
+(d) : print("x+1")
+# its gonna show exactly whats inside our double qoutation as a string
+# (e) : print('x'+1)
+# part (e) is a wrong line of code ,we cant do math operations on a string , doesn't make sense
+(f) : print(x+1)
+# 2+1=3
